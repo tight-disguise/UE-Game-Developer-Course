89b30baa40c9425c18a7341d975c16d3767e6e9a
tight-disguise <mackie@outlook.com>
2024-11-03 09:59:35 +0100

    Added 03. Complex Number Class.cpp and VS project files.

---
+++ 03. Complex Number Class/03. Complex Number Class.cpp
@@ -0,0 +1,26 @@
+/* 03. Complex Number Class.cpp
+*
+ * Unreal Engine Game Developer Course | Laba.IT
+ *
+ * Home Assignment No. 3:
+ * - write a class representing complex numbers
+ * - class should contain:
+ *   - default, parametric & copying constructor
+ *   - destructor
+ *   - overloaded operators: +, -, *, /, =
+ *   - Print() method to display complex number
+ * - main() function should implement a set of tests of implemented complex number class & operations.
+ * 
+ * Code written by Hubert Pitura (hpitura@gmail.com)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Complex numbers, here I come!!!" << endl;
+    
+    return 0;
+}
